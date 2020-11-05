462a3268ac0c555c477eff91d9ccee06b80c2bfe
Ahmed <imaneben8@gmail.com>
2020-11-05 12:21:20 +0000

    colocate state

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,13 @@
 
 import React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input id="name" value={name} onChange={event => setName(event.target.value)} />
     </div>
   )
 }
@@ -27,8 +29,12 @@ function FavoriteAnimal({animal, onAnimalChange}) {
 }
 
 
-function Display({name, animal}) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+// function Display({animal}) {
+//   return <div>{`Your favorite animal is: ${animal}!`}</div>
+// }
+
+function Display({animal}) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>
 }
 
 
@@ -36,13 +42,12 @@ function Display({name, animal}) {
 function App() {
   // 🐨 add a useState for the animal
   const [animal, setAnimal] = React.useState('')
-  const [name, setName] = React.useState('')
   return (
     <form>
     
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name  />
       <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)}  />
-      <Display name={name} animal={animal}/>
+      <Display  animal={animal}/>
     </form>
   )
 }
